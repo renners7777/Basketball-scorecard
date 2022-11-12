8dec7410b17b6bce49c065a338e7d3446cfe8a40
Chris Renshaw <chrisrenshaw79@gmail.com>
2022-11-12 21:58:26 +0000

    all files updated

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
 let homeStoreEl = document.getElementById("home-score")
 let homeScore = 0
 let awayScoreBtnOne = document.getElementById("away-score-btn-1")
+let awayScoreBtnTwo = document.getElementById("away-score-btn-2")
+let awayScoreBtnThree = document.getElementById("away-score-btn-3")
 let awayStoreEl = document.getElementById("away-score")
 let awayScore = 0
 
@@ -10,24 +14,24 @@ function increaseHomeScoreOne() {
     homeStoreEl.textContent = homeScore
 }
 
-function increaseAwayScoreOne() {
-    awayScore += 1
-    awayStoreEl.textContent = awayScore
-}
-
 function increaseHomeScoreTwo() {
     homeScore += 2
     homeStoreEl.textContent = homeScore
 }
 
-function increaseAwayScoreTwo() {
-    awayScore += 2
-    awayStoreEl.textContent = awayScore
-}
-
 function increaseHomeScoreThree() {
     homeScore += 3
     homeStoreEl.textContent = homeScore
+}
+
+function increaseAwayScoreOne() {
+    awayScore += 1
+    awayStoreEl.textContent = awayScore
+}
+
+function increaseAwayScoreTwo() {
+    awayScore += 2
+    awayStoreEl.textContent = awayScore
 }
 
 function increaseAwayScoreThree() {
